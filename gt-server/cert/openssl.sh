f1d4bf3982000c6be27581fd235fe045a5575a47
SilvaOrdep <pedro.o.silva975@gmail.com>
2025-02-27 16:28:20 -0300

    Remove pastas indesejadas do índice

--- gt-server/cert/openssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#openssl genrsa -out key.pem
-#openssl req -new -key key.pem -out csr.pem
-#openssl x509 -req -signkey key.pem -in csr.pem -out certificate.crt
-
-openssl req -x509 -nodes -days 36500 -newkey rsa:2048 -keyout key.pem -out certificate.crt
